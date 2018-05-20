b6332ce098b8a8120690abdef7d60db9e0c22d57
Brad Newman <newmanbradm@gmail.com>
2018-05-20 19:15:27 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,20 @@
 def consolidate_cart(cart)
-  # code here
+  cart_hash = {}
+  
+  cart.each do |item|
+    item.each do |type, values|
+      if cart_hash.include?(type)
+        cart_hash[type][:count] += 1
+      else
+        cart_hash[type] = {}
+        values.each do |key, value|
+          cart_hash[type][key] = value
+          cart_hash[type][:count] = 1
+        end
+      end
+    end
+  end
+  cart_hash
 end
 
 def apply_coupons(cart, coupons)
